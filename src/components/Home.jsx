ab7f7a123c99d24cbd5df2eea71738736399bc57
Vania Venegas <venegas.vania@gmail.com>
2021-10-26 11:03:41 -0300

    arregalando errores

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,10 @@ const modalStyles = {
   content: {
     top: '50%',
     left: '50%',
-    right: '10%',
+    right: 'auto',
     bottom: 'auto',
-    marginRight: '-30%',
+    marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-
   },
 };
 
@@ -47,19 +46,6 @@ const Home = (props) => {
   }
 
   return (
-<<<<<<< HEAD
-    <div >
-      <div className="containerHome">
-        <div>
-          <div>
-            <img className="bhpLogo" src="https://firebasestorage.googleapis.com/v0/b/elc-bhp.appspot.com/o/bhp_orn_rgb_pos%5B1%5D.png?alt=media&token=e67982c5-67f2-4153-9201-a18c75117eff" width="100px"/>
-          </div>
-          <div className="backgroundImg">
-            <img className="" src="https://i.pinimg.com/564x/27/d9/71/27d97141788c1c2f604e1d2949f0390a.jpg"/>
-          </div>        
-        </div>
-
-=======
     <div className="containerHome" >
       <div className="imgDesktop"> 
         <img  src=""  alt=""/>
@@ -68,7 +54,6 @@ const Home = (props) => {
         <nav className="bhpLogo">
           <img  src="https://firebasestorage.googleapis.com/v0/b/elc-bhp.appspot.com/o/bhp_orn_rgb_pos%5B1%5D.png?alt=media&token=e67982c5-67f2-4153-9201-a18c75117eff" alt="logo" width="100px" />
         </nav>
->>>>>>> 69cbdaaf3c8b3147e5672f8c532007df9b693b3f
         <div className="keepSafe">
           <h1 className="nameApp">ESTANDAR DE DETENCIÓN DE CAÍDA</h1>
           <h1 className="textKeepSafe">En casa te esperan, mantente seguro</h1>
@@ -91,20 +76,10 @@ const Home = (props) => {
             </select>
           </div>
 
-<<<<<<< HEAD
-          
-        </div>
-        <div className="modal">
-        <Modal isOpen={modalIsOpen} style={modalStyles}>
-          <h1>Acuerdo de licencia de usuario final</h1>
-          <p>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.</p>
-          <p>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.</p>
-=======
           <button type="buttonHome" onClick={handleOpenModal} className="btn btn-secondary">CONTINUAR</button>
         </div>
         
           <Modal isOpen={modalIsOpen} style={modalStyles}>
->>>>>>> 69cbdaaf3c8b3147e5672f8c532007df9b693b3f
           <div>
             <h1>Acuerdo de licencia de usuario final</h1>
             <p>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.</p>
